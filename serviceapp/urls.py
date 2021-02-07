843d288528047dbfaab05897f9da028bf8e16cf9
gogora6 <lasha.gogorishvili.1@btu.edu.ge>
2021-02-08 00:38:41 +0400

    added order details template and views

--- serviceapp/urls.py
+++ serviceapp/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import index, washer_list, washer_detail, create_order
+from .views import index, washer_list, washer_detail, create_order, order_detail
 
 app_name = 'wash'
 urlpatterns = [
     path('', index, name='index'),
     path('washers/', washer_list, name='washers'),
     path('<int:pk>/washer/', washer_detail, name='washer_detail'),
-    path('order/create', create_order, name='create_order')
+    path('order/create', create_order, name='create_order'),
+    path('<int:pk>/order/', order_detail, name='order_detail')
 ]
--- serviceapp/views.py
+++ serviceapp/views.py
@@ -1,17 +1,19 @@
+from django.core.handlers.wsgi import WSGIRequest
 from django.http import HttpResponse
-from django.shortcuts import render, get_object_or_404
-from django.db.models import Sum, Q
+from django.shortcuts import render, get_object_or_404, redirect
+from django.utils import timezone
+from django.db.models import Sum
+
+from .models import Order, Car, WashType
 from user.choices import Status
 from user.models import User
-from .models import Order, Car, WashType
-from django.utils import timezone
 
 
-def index(request):
+def index(request: WSGIRequest) -> HttpResponse:
     return render(request, template_name='pages/index.html')
 
 
-def washer_list(request):
+def washer_list(request: WSGIRequest) -> HttpResponse:
     washers = User.objects.filter(status=Status.washer).all()
 
     return render(request, template_name='pages/washers.html',
@@ -20,7 +22,7 @@ def washer_list(request):
                   })
 
 
-def washer_detail(request, pk):
+def washer_detail(request: WSGIRequest, pk: int) -> HttpResponse:
     washer = get_object_or_404(User.objects.filter(status=Status.washer.value), pk=pk)
 
     filter_day = request.GET.get('filter')
@@ -29,9 +31,9 @@ def washer_detail(request, pk):
 
     if filter_day:
         filter_date = now - timezone.timedelta(days=int(filter_day))
-        orders = Order.objects.filter(employee=pk, start_date__gte=filter_date)
+        orders = Order.objects.filter(employee=pk, end_date__isnull=False, start_date__gte=filter_date)
     else:
-        orders = Order.objects.filter(employee=pk)
+        orders = Order.objects.filter(employee=pk, end_date__isnull=False)
 
     bonus = 0
     if orders.exists():
@@ -46,14 +48,22 @@ def washer_detail(request, pk):
     })
 
 
-def create_order(request):
+def order_detail(request: WSGIRequest, pk: int) -> HttpResponse:
+    order = get_object_or_404(Order, pk=pk)
+
+    return render(request, template_name='pages/order_details.html', context={
+        'order': order
+    })
+
+
+def create_order(request: WSGIRequest) -> HttpResponse:
     car_list = Car.objects.all()
     wash_types = WashType.objects.all()
     washers = User.objects.filter(status=Status.washer).all()
 
     if request.method == 'POST':
         data = request.POST
-        Order.objects.create(
+        new_order = Order.objects.create(
             employee_id=data.get('washer'),
             car_id=data.get('car'),
             wash_type_id=data.get('wash_type'),
@@ -61,7 +71,7 @@ def create_order(request):
             note=data.get('note'),
             start_date=data.get('wash_date')
         )
-        return HttpResponse("Order Created")
+        return redirect('wash:order_detail', new_order.pk)
     return render(request,
                   'pages/order-form.html',
                   context={'car_list': car_list, 'wash_types': wash_types, 'washers': washers})
