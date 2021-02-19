d89c284d756fd4340228850d81536277c7aa3144
gogora6 <lasha.gogorishvili.1@btu.edu.ge>
2021-02-19 17:40:40 +0400

    updated birthdate validatior

--- user/validators.py
+++ user/validators.py
@@ -3,8 +3,8 @@ from django.utils.translation import gettext_lazy as _
 import datetime
 
 
-def validate_birthdate(birthdate: str) -> None:
-    if birthdate > datetime.date.today():
+def validate_birthdate(value: datetime) -> None:
+    if value > datetime.date.today():
         raise ValidationError(
             _('Birthdate cannot be in the past!'),
         )
